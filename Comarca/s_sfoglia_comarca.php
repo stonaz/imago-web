80393abfab64365ee3a0aa0bb94d2065c28a38d1
root <root@debian11.localdomain>
2022-06-30 13:35:01 +0200

    comarca

--- Comarca/s_sfoglia_comarca.php
+++ Comarca/s_sfoglia_comarca.php
@@ -12,7 +12,7 @@ if (!isset($_GET['dir'])){$dir="Comarca";}
 if (!isset($_GET['r'])){$row="COMARCA-0";}
 	else {$row=$_GET['r'];}
 	
-$dbconn = pg_connect ("host=$dbserver port=5432 dbname=comarca user=imago_web password=normal.2020") or die ('no db');
+$dbconn = pg_connect ("host=$dbserver port=5432 dbname=comarca user=$user password=$pwd") or die ('no db');
 $query="select * from mappe where file = '$row' ;";
 
 
@@ -26,17 +26,17 @@ while($record=pg_fetch_array($result))
 }
 //echo $descr;
 
-function mostra($descr,$row,$dir,$dbserver,$serverIIP)
+function mostra($descr,$row,$dir,$dbserver,$serverIIP,$root)
 {
 	$file=$row;
 	global $catalogo;
 	echo "<CENTER><strong>$file</strong><br>$descr<br><A  onMouseOver=\"this.style.cursor='pointer'\" onMouseOut=\"this.style.cursor='text'\" onClick=\"javascript:immv('";
 	echo $file.".jp2','".$dir."')\" BORDER=0>";
-    	echo "<IMG SRC=\"http://".$serverIIP."/iiifserver?FIF=/images/Patrimonio/Archivi/AS_Roma/Imago/".$dir."/".$file.".jp2&SDS=0,90&CNT=1.0&WID=800&QLT=100&CVT=jpeg\">";
+    	echo "<IMG SRC=\"http://".$serverIIP."/iiifserver?FIF=$root".$dir."/".$file.".jp2&SDS=0,90&CNT=1.0&WID=800&QLT=100&CVT=jpeg\">";
 	print'</A></CENTER>';
 	
 }
-mostra($descr,$row,$dir,$dbserver,$serverIIP);
+mostra($descr,$row,$dir,$dbserver,$serverIIP,$root);
 
 ?>
 
@@ -47,7 +47,7 @@ mostra($descr,$row,$dir,$dbserver,$serverIIP);
 function immv(file,dir)
 {
 	var path = dir + '/' + file ;
-	url="http://<?PHP echo $serverIIP ?>/iip_viewer/<?PHP echo $viewer ?>?dir=/AS_Roma/Imago/&file=" +path ;
+	url="http://<?PHP echo $serverIIP ?>/iip_viewer/<?PHP echo $viewer ?>?dir=&file=" +path ;
 	window.open(url,null, "height=400,width=600,status=yes,toolbar=no,menubar=no,location=no");
 }
 
