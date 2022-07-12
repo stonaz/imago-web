ed2edbdd5b80b126b48911728d6afffae0c34360
root <root@debian11.localdomain>
2022-07-12 11:17:02 +0200

    Cartografica-IP-URL

--- cartografica/js/lista_segnature_collezioni_var.js
+++ cartografica/js/lista_segnature_collezioni_var.js
@@ -1,9 +1,10 @@
 
    //var segnatura = [80, 239, 1];
    
-   var SERVER_URL="http://10.211.55.14/";
-   var IIP_URL="http://10.211.55.14/";
-   var url = SERVER_URL + "cartografica/php/segnature_main_collezioni.php" ;
+   var SERVER_URL="http://212.189.172.101/";
+   var IIP_URL="http://212.189.172.101/";
+   var url = SERVER_URL + "/cartografica/php/segnature_main_collezioni.php" ;
+   console.log(url);
     var lista_segnature_collezioni = $.ajax({
         async: false,
         url: url,
--- cartografica/php/conn.php
+++ cartografica/php/conn.php
@@ -1,3 +1,4 @@
 <?PHP
-$dbconn = pg_connect ("host=$dbserver port=5432 dbname=Cartografica user=imago_web password=normal.2020") or die ('no db');
+require "../../parametri.php";
+$dbconn = pg_connect ("host=$dbserver port=5432 dbname=Cartografica user=$user password=$pwd") or die ('no db');
 ?>
