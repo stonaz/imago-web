032c84febcea2f16a84c85d365d478e7089be851
root <root@debian11.localdomain>
2022-07-13 10:52:18 +0200

    connessione inventari

--- Inventari/conn.php
+++ Inventari/conn.php
@@ -1,3 +1,4 @@
 <?PHP
-$dbconn = pg_connect ("host=$dbserver port=5432 dbname=inventari2020 user=imago_web password=normal.2020") or die ('no db');
+include "../parametri.php";
+$dbconn = pg_connect ("host=$dbserver port=5432 dbname=inventari2020 user=$user password=$pwd") or die ('no db');
 ?>
