8d39bb1207f008d97866a77ba1f6bf370a06b0b2
root <root@debian11.localdomain>
2022-08-08 18:31:00 +0200

    discriminazioni

--- discriminazioni/conn.php
+++ discriminazioni/conn.php
@@ -3,11 +3,12 @@
         require 'check_auth.php';
         $dbconn = pg_connect ("host=$dbserver port=5432 dbname=Ebrei user=postgres password=Superman123") or die ('no db');
        // $root_dir = "/images/AS_Roma/Patrimonio/Imago/discriminazioni/";
-        $root_dir="\\\\192.168.2.5\\imago\\Patrimonio\\Archivi\\AS_Roma\\Imago\\Discriminazioni";
+      //  $root_dir="\\\\192.168.2.5\\imago\\Patrimonio\\Archivi\\AS_Roma\\Imago\\Discriminazioni";
       //  $iip_dir = "/images/AS_Roma/Patrimonio/Imago/discriminazioni/";
         $iip_dir = "/images/Patrimonio/Archivi/AS_Roma/Imago/Discriminazioni/";
 
-        $server = "www.imago.archiviodistatoroma.beniculturali.it";
+       // $server = "www.imago.archiviodistatoroma.beniculturali.it";
+        $server = "212.189.172.101";
         try {
    $pdo = new PDO('pgsql:host='.$dbserver.';port=5432;dbname=Ebrei;user=postgres;password=Superman123');
    //echo "PDO connection object created";
