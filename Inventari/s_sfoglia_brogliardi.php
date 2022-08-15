5ef208faf50b4e1f94cfd74cf52a1df1d6523113
root <root@debian11.ste.net>
2022-08-15 19:45:05 +0200

    Inventari

--- Inventari/s_sfoglia_brogliardi.php
+++ Inventari/s_sfoglia_brogliardi.php
@@ -15,19 +15,19 @@ if (!isset($_GET['r1'])){$row="01.jp2";}
 
 
 
-function mostra($row,$dir,$dbserver,$serverIIP)
+function mostra($row,$dir,$dbserver,$serverIIP,$root)
 {
 	$file=$row;
 	//echo $file;
 	global $catalogo;
 	echo "<CENTER><A  onMouseOver=\"this.style.cursor='pointer'\" onMouseOut=\"this.style.cursor='text'\" onClick=\"javascript:immv('";
 	echo $file."','".$dir."')\" BORDER=0>";
-	echo "<IMG SRC=\"http://".$serverIIP."/iiifserver?FIF=/images/Patrimonio/Archivi/AS_Roma/Imago/".$dir."/".$file."&SDS=0,90&CNT=1.0&WID=512&QLT=100&CVT=jpeg\">";
+	echo "<IMG SRC=\"http://".$serverIIP."/iipsrv/iipsrv.fcgi?FIF=$root".$dir."/".$file."&SDS=0,90&CNT=1.0&WID=512&QLT=100&CVT=jpeg\">";
 	//echo "<IMG SRC=\"http://".$host."/lizardtech/iserv/getthumb?cat=".$catalogo."&item=".$dir."\\".$file."&thumbspec=bigger\">";
 	print'</A></CENTER>';
 	
 }
-mostra($row,$dir,$dbserver,$serverIIP);
+mostra($row,$dir,$dbserver,$serverIIP,$root);
 
 ?>
 
@@ -41,7 +41,7 @@ function immv(file,dir)
 	var path = dir + '/' + file ;
 	//url_inizio="http://<?PHP echo $serverIIP; ?>:9001/StyleServer/calcrgn?browser=win_ie&cat=Imago&style=default/view.xsl&wid=400&hei=300&browser=win_ie&plugin=false&item=";
 	//url_fine="&wid=400&hei=300&style=default/view.xsl&plugin=false";
-	url="http://<?PHP echo $serverIIP ?>/iip_viewer/<?PHP echo $viewer ?>?dir=/AS_Roma/Imago/&file=" +path ;
+	url="http://<?PHP echo $serverIIP ?>/iip_viewer/<?PHP echo $viewer ?>?dir=&file=" +path ;
 	window.open(url,null, "height=400,width=600,status=yes,toolbar=no,menubar=no,location=no");
 	
 }
