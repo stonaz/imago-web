7d912784ca303ce8e20d2e86199ed2616a354f74
root <root@debian11.ste.net>
2022-10-31 18:33:05 +0100

    discriminazioni nuova_URL

--- discriminazioni/conn.php
+++ discriminazioni/conn.php
@@ -10,7 +10,7 @@
         $iip_dir = "/images/Patrimonio/Archivi/AS_Roma/Imago/Discriminazioni/";
 
        // $server = "www.imago.archiviodistatoroma.beniculturali.it";
-        $server = "212.189.172.101";
+        $server = "imagoarchiviodistatoroma.cultura.gov.it";
         try {
    $pdo = new PDO('pgsql:host='.$dbserver.';port=5432;dbname=Ebrei;user=postgres;password=Superman123');
    //echo "PDO connection object created";
